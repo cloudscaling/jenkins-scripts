dc41893201b23f0c5230582e51ca52959bf6b0f8
Andrey <andrey-mp@yandex.ru>
2016-04-23 10:23:46 +0300

    juju: update 1-3-5-3 scenario. rmove slave instead master

--- juju/scaleio/check-mdm-cluster.sh
+++ juju/scaleio/check-mdm-cluster.sh
@@ -65,9 +65,9 @@ if wait_and_check 1 ; then
       echo "------------------------------------------- Scale MDM's count back to 3 ---"
       echo "---------------------------------------------------------------------------"
       output=`juju ssh $master_mdm sudo scli --query_cluster --approve_certificate`
-      mdm1='scaleio-mdm/'`echo "$output" | grep -A 1 "Master MDM" | grep "Name:" | awk '{print $2}' | sed "s/.*\([0-9]\),/\1/"`
+      mdm1='scaleio-mdm/'`echo "$output" | grep -A 1 "Slave MDMs" | grep "Name:" | awk '{print $2}' | sed "s/.*\([0-9]\),/\1/"`
       mdm2='scaleio-mdm/'`echo "$output" | grep -A 1 "Tie-Breakers" | grep "Name:" | awk '{print $2}' | sed "s/.*\([0-9]\),/\1/"`
-      echo "Removing Master MDM: $mdm1"
+      echo "Removing Slave MDM: $mdm1"
       juju remove-unit $mdm1
       echo "Removing Tie-Breaker: $mdm2"
       juju remove-unit $mdm2
