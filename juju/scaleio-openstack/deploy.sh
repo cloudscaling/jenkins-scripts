e506f5ac5fffb8a4425729b178e4b411a2a5a96c
Andrey Pavlov <andrey-mp@yandex.ru>
2016-05-24 16:30:11 +0300

    remove hacking Juju's main security group in EC2

--- juju/scaleio-openstack/deploy.sh
+++ juju/scaleio-openstack/deploy.sh
@@ -25,12 +25,6 @@ echo "Wait for services start: $(date)"
 wait_absence_status_for_services "executing|blocked|waiting"
 echo "Wait for services end: $(date)"
 
-# fix security group 'juju-amazon'
-# TODO: another bug somewhere
-group_id=`aws ec2 describe-security-groups --group-name juju-amazon --query 'SecurityGroups[0].GroupId' --output text`
-aws ec2 revoke-security-group-ingress --group-name juju-amazon --protocol tcp  --port 0-65535 --source-group $group_id
-aws ec2 authorize-security-group-ingress --group-name juju-amazon --protocol tcp --cidr "0.0.0.0/0" --port 0-65535
-
 # check for errors
 if juju status | grep "current" | grep error ; then
   echo "ERROR: Some services went to error state"
