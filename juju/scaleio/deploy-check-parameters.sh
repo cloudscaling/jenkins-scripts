45e0733cef6708afbf9a84284f5ec9aa3ad72800
tikitavi <rtikitavi@gmail.com>
2016-06-10 19:44:10 +0300

    moved instance-type and zone determination to functions

--- juju/scaleio/deploy-check-parameters.sh
+++ juju/scaleio/deploy-check-parameters.sh
@@ -12,15 +12,15 @@ export PASSWORD="Default_password"
 echo "INFO: start $(date)"
 
 echo "Create machines"
-m1=$(juju add-machine --constraints "instance-type=t2.medium" 2>&1 | awk '{print $3}')
+m1=$(juju add-machine --constraints "$INSTANCE_TYPE" 2>&1 | awk '{print $3}')
 echo "Machine created: $m1"
-m2=$(juju add-machine --constraints "instance-type=t2.medium" 2>&1 | awk '{print $3}')
+m2=$(juju add-machine --constraints "$INSTANCE_TYPE" 2>&1 | awk '{print $3}')
 echo "Machine created: $m2"
-m3=$(juju add-machine --constraints "instance-type=t2.medium" 2>&1 | awk '{print $3}')
+m3=$(juju add-machine --constraints "$INSTANCE_TYPE" 2>&1 | awk '{print $3}')
 echo "Machine created: $m3"
-#m4=$(juju add-machine --constraints "instance-type=t2.medium" 2>&1 | awk '{print $3}')
+#m4=$(juju add-machine --constraints "$INSTANCE_TYPE" 2>&1 | awk '{print $3}')
 #echo "Machine created: $m4"
-#m5=$(juju add-machine --constraints "instance-type=t2.medium" 2>&1 | awk '{print $3}')
+#m5=$(juju add-machine --constraints "$INSTANCE_TYPE" 2>&1 | awk '{print $3}')
 #echo "Machine created: $m5"
 
 wait_for_machines $m1 $m2 $m3 #$m4 $m5
@@ -30,9 +30,8 @@ instances=`juju status | grep instance-id | awk '{print$2}'`
 i=0
 for instance in $instances ; do
   if [[ $i != 0 ]] ; then
-    zone=`aws ec2 describe-instance-status --instance-ids $instance | grep "AvailabilityZone" | sed 's/[,\"]//g' | awk '{print$2}'`
-    create_attach_volume $instance 100 "/dev/xvdf" $zone
-    create_attach_volume $instance 100 "/dev/xvdg" $zone
+    create_attach_volume $instance 100 "/dev/xvdf"
+    create_attach_volume $instance 100 "/dev/xvdg"
   fi
   ((++i))
 done
