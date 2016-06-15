57bbe30353d401871cd462e34a1b20f703affaaf
alexey-mr <alexey.morlang@gmail.com>
2016-06-15 15:45:33 +0300

    Added checks for zero-padding

--- juju/fuel/deploy-fuel.sh
+++ juju/fuel/deploy-fuel.sh
@@ -1,14 +1,28 @@
 #!/bin/bash -e
 
-source functions
+my_file="$(readlink -e "$0")"
+my_dir="$(dirname $my_file)"
+
+source $my_dir/functions
+
 
 # provision machines
-provision_machines 0 1 2 3 4
+provision_machines 0 1 2 3
 
 # prepare fuel master
 prepare_fuel_master 0
 
-# 3+1 cluster
-configure_cluster mode 3 primary-controller 1,2,3 compute 4
+# 3+1 cluster with default parameters:
+#   zero-padding=false
+#   TODO: add more parameters and check their default value
+configure_cluster mode 1 primary-controller 1 compute 2,3
+check_storage_pool 1 'Zero padding' 'disabled'   
+
+remove_node_service 1 2 3 4
+#TODO: add more key=value in set_fuel_options for more parameters with different (from default_ values)
+set_fuel_options zero-padding=true
+configure_cluster mode 1 primary-controller 1 compute 2,3
+check_storage_pool 1 'Zero padding' 'enabled'   
+
 
 save_logs
