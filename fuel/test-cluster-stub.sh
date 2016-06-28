afc4b0e04b5dd09825cda5d9184d7f2ca53287dc
alexey-mr <alexey.morlang@gmail.com>
2016-06-28 17:34:33 +0300

    fuel-ci fixed path to copy files into fuel master

--- fuel/test-cluster-stub.sh
+++ fuel/test-cluster-stub.sh
@@ -6,7 +6,7 @@ my_dir="$(dirname $my_file)"
 
 #TODO: change it after reuse of other provisioning:
 fuel_master='10.20.0.2'
-scp -o StrictHostKeyChecking=no ${my_dir}/*.sh root@${fuel_master}
-scp -o StrictHostKeyChecking=no ${my_dir}/*.py root@${fuel_master}
+scp -o StrictHostKeyChecking=no ${my_dir}/*.sh root@${fuel_master}:/root/
+scp -o StrictHostKeyChecking=no ${my_dir}/*.py root@${fuel_master}:/root/
 ssh -o StrictHostKeyChecking=no root@${fuel_master} '~/prepare_plugin.sh'
 ssh -o StrictHostKeyChecking=no root@${fuel_master} '~/test-cluster.sh'
