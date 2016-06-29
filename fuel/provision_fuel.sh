ed6544be62fd6ed213d17b0c125bf49356ad0037
alexey-mr <alexey.morlang@gmail.com>
2016-06-29 15:41:49 +0300

    move cleanup of known_hosts before deploy

--- fuel/provision_fuel.sh
+++ fuel/provision_fuel.sh
@@ -13,6 +13,14 @@ fi
 
 fuel_master='10.20.0.2'
 
+if [ -f /home/jenkins/.ssh/known_hosts ] ; then
+  ssh-keygen -f /home/jenkins/.ssh/known_hosts  -R $fuel_master
+fi
+
+if [ -f /root/.ssh/known_hosts ] ; then
+  ssh-keygen -f /root/.ssh/known_hosts  -R $fuel_master
+fi
+
 pushd ${my_dir}/fuel-kvm
   if ! ./deploy_fuel.sh ${my_dir}/${mos} ${nodes} ; then
     echo ERROR: failed to deploy environment
@@ -21,17 +29,10 @@ pushd ${my_dir}/fuel-kvm
   fi
 popd
 
-if [ -f /home/jenkins/.ssh/known_hosts ] ; then
-  ssh-keygen -f /home/jenkins/.ssh/known_hosts  -R 10.20.0.2
-fi
-
-if [ -f /root/.ssh/known_hosts ] ; then
-  ssh-keygen -f /root/.ssh/known_hosts  -R 10.20.0.2
-fi
-
+ssh_opts='-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null'
 id_rsa_pub_key=$(cat /home/jenkins/.ssh/id_rsa.pub)
 ssh_cmd="mkdir -p ~/.ssh && chmod 0644 ~/.ssh && echo ${id_rsa_pub_key} > ~/.ssh/authorized_keys && chmod 0644 ~/.ssh/authorized_keys"
-offending_key=`sshpass -pr00tme ssh -o StrictHostKeyChecking=no root@${fuel_master} ${ssh_cmd} 2>&1 | awk -F ':' '/Offending ECDSA key/ {print($2)}'`
+offending_key=`sshpass -pr00tme ssh ${ssh_opts} root@${fuel_master} ${ssh_cmd} 2>&1 | awk -F ':' '/Offending ECDSA key/ {print($2)}'`
 if [[ $? != 0 ]] ; then
   if [ -n "${offending_key}" ] ; then
     sed -i "${offending_key}d" ~/.ssh/known_hosts
@@ -41,7 +42,7 @@ if [[ $? != 0 ]] ; then
     exit -1
   fi
   # retry
-  if ! sshpass -pr00tme ssh -o StrictHostKeyChecking=no root@${fuel_master} ${ssh_cmd} ; then
+  if ! sshpass -pr00tme ssh ${ssh_opts} root@${fuel_master} ${ssh_cmd} ; then
     echo ERROR: failed to add ssh key to fuel known hosts
     exit -1
   fi
