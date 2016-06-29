3f38adba847c42a64c58c517441ef3dce83c9981
tikitavi <rtikitavi@gmail.com>
2016-06-29 17:33:21 +0300

    added checks for protection domain, storage pool, device path, moved get_config to common functions

--- juju/fuel/deploy-fuel.sh
+++ juju/fuel/deploy-fuel.sh
@@ -5,7 +5,6 @@ my_dir="$(dirname $my_file)"
 
 source $my_dir/functions
 
-
 # provision machines
 provision_machines 0 1 2 3
 
@@ -17,6 +16,9 @@ prepare_fuel_master 0
 #   TODO: add more parameters and check their default value
 configure_cluster mode 1 primary-controller 1 compute 2,3
 
+check_protection_domain 1 'default'
+check_sp_name 1 'default'
+check_path 1 'Device ' '/dev/xvdf'
 check_storage_pool 1 'Zero padding' 'disabled'
 check_storage_pool 1 'Checksum mode' 'disabled'
 check_storage_pool 1 'Background device scanner' 'Disabled'
@@ -25,24 +27,31 @@ check_capacity_alerts 1 '80' '90'
 check_storage_pool 1 'Flash Read Cache' "Doesn't use"
 
 remove_node_service 1 2 3
-
 #TODO: add more key=value in set_fuel_options for more parameters with different (from default_ values)
-set_fuel_options zero-padding=true
-set_fuel_options checksum-mode=true
-set_fuel_options scanner-mode=true
-set_fuel_options spare-policy=15
-set_fuel_options capacity-high-alert-threshold=79
-set_fuel_options capacity-critical-alert-threshold=89
-set_fuel_options cached-storage-pools=default
-set_fuel_options rfcache-devices=/dev/xvdg
+set_fuel_options protection-domain='pd'
+set_fuel_options storage-pools='sp'
+set_fuel_options device-paths='/dev/xvdg'
+set_fuel_options password='Other_password'
+set_fuel_options zero-padding='true'
+set_fuel_options checksum-mode='true'
+set_fuel_options scanner-mode='true'
+set_fuel_options spare-policy='15'
+set_fuel_options capacity-high-alert-threshold='79'
+set_fuel_options capacity-critical-alert-threshold='89'
+set_fuel_options cached-storage-pools='sp'
+set_fuel_options rfcache-devices='/dev/xvdf'
 configure_cluster mode 1 primary-controller 1 compute 2,3
 
+check_password 1 'Other_password'
+check_protection_domain 1 'pd'
+check_sp_name 1 'sp'
+check_path 1 'Device ' '/dev/xvdg'
 check_storage_pool 1 'Zero padding' 'enabled'
 check_storage_pool 1 'Checksum mode' 'enabled'
 check_storage_pool 1 'Background device scanner' 'Mode: device_only'
 check_storage_pool 1 'Spare policy' '15%'
 check_capacity_alerts 1 '79' '89'
 check_storage_pool 1 'Flash Read Cache' "Uses"
-check_cache 1 '/dev/xvdg'
+check_path 1 'Rfcache device ' '/dev/xvdf'
 
 save_logs
