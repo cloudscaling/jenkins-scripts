9650518bffafbbc0bf203bba22329a6f7a17429b
tikitavi <rtikitavi@gmail.com>
2016-06-30 09:00:22 +0300

    edit indents, changed back instance type

--- juju/fuel/deploy-fuel.sh
+++ juju/fuel/deploy-fuel.sh
@@ -11,6 +11,9 @@ provision_machines 0 1 2 3
 # prepare fuel master
 prepare_fuel_master 0
 
+disk1="/dev/xvdb"
+disk2="/dev/xvdc"
+
 # 3+1 cluster with default parameters:
 #   zero-padding=false
 #   TODO: add more parameters and check their default value
@@ -18,7 +21,7 @@ configure_cluster mode 1 primary-controller 1 compute 2,3
 
 check_protection_domain 1 'default'
 check_sp_name 1 'default'
-check_path 1 'Device ' '/dev/xvdf'
+check_path 1 'Device ' $disk1
 check_storage_pool 1 'Zero padding' 'disabled'
 check_storage_pool 1 'Checksum mode' 'disabled'
 check_storage_pool 1 'Background device scanner' 'Disabled'
@@ -27,10 +30,9 @@ check_capacity_alerts 1 '80' '90'
 check_storage_pool 1 'Flash Read Cache' "Doesn't use"
 
 remove_node_service 1 2 3
-#TODO: add more key=value in set_fuel_options for more parameters with different (from default_ values)
 set_fuel_options protection-domain='pd'
 set_fuel_options storage-pools='sp'
-set_fuel_options device-paths='/dev/xvdg'
+set_fuel_options device-paths=$disk2
 set_fuel_options password='Other_password'
 set_fuel_options zero-padding='true'
 set_fuel_options checksum-mode='true'
@@ -39,19 +41,19 @@ set_fuel_options spare-policy='15'
 set_fuel_options capacity-high-alert-threshold='79'
 set_fuel_options capacity-critical-alert-threshold='89'
 set_fuel_options cached-storage-pools='sp'
-set_fuel_options rfcache-devices='/dev/xvdf'
+set_fuel_options rfcache-devices=$disk1
 configure_cluster mode 1 primary-controller 1 compute 2,3
 
 check_password 1 'Other_password'
 check_protection_domain 1 'pd'
 check_sp_name 1 'sp'
-check_path 1 'Device ' '/dev/xvdg'
+check_path 1 'Device ' $disk2
 check_storage_pool 1 'Zero padding' 'enabled'
 check_storage_pool 1 'Checksum mode' 'enabled'
 check_storage_pool 1 'Background device scanner' 'Mode: device_only'
 check_storage_pool 1 'Spare policy' '15%'
 check_capacity_alerts 1 '79' '89'
 check_storage_pool 1 'Flash Read Cache' "Uses"
-check_path 1 'Rfcache device ' '/dev/xvdf'
+check_path 1 'Rfcache device ' $disk1
 
 save_logs
