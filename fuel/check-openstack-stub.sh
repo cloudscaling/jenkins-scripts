1a5f293b53e4b745f1749cd725b8b104df6a8dd1
alexey-mr <alexey.morlang@gmail.com>
2016-07-04 20:53:06 +0300

    fuel-ci fix misprints

--- fuel/check-openstack-stub.sh
+++ fuel/check-openstack-stub.sh
@@ -6,5 +6,5 @@ my_dir="$(dirname $my_file)"
 source ${my_dir}/fuel-utils
 
 controller_node=`get_controller_nodes | head -n 1`
-copy_no_slave $controller_node "${my_dir}/check-openstack.sh ${my_dir}/../juju/functions-openstack"
+copy_to_slave $controller_node "${my_dir}/check-openstack.sh ${my_dir}/../juju/functions-openstack"
 execute_on_slave $controller_node "./check-openstack.sh"
