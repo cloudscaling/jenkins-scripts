395a67fd9be200343359c2a1c8fa01244ae6d324
alexey-mr <alexey.morlang@gmail.com>
2016-07-04 19:56:04 +0300

    fuel-ci fixed run of scli

--- fuel/check-openstack.sh
+++ fuel/check-openstack.sh
@@ -8,11 +8,16 @@ source /etc/environment
 source $my_dir/functions-openstack
 
 function exec_on_mdm() {
-  $@
+  bash -c "$@"
 }
 
 function get_provisioning_type() {
-  awk '/provisioning_type:/ {print($2)}' /etc/astute.yaml
+  type=`awk '/provisioning_type:/ {print($2)}' /etc/astute.yaml` 2>/dev/null
+  if [[ "${type}" != 'thin' ]] ; then
+    echo Thick
+  else
+    echo Thin
+  fi
 }
 
 easy_install pip
