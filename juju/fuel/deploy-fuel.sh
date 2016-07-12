bab9651c427fd247eb535e7ba73283bed515b3f8
tikitavi <rtikitavi@gmail.com>
2016-07-12 18:37:20 +0400

    added protection-domain-nodes test, changed tests to check several protection domains (#24)
    * added protection-domain-nodes test, changed tests to check several protection domains
    
    * some fixes, added function wait_and_check
    
    * removed add_computes back
    
    * added note to adding new compute node in default state
    
    * added function get_pd_list, changed adding compute nodes
    
    * changed get_pd_list function

--- juju/fuel/deploy-fuel.sh
+++ juju/fuel/deploy-fuel.sh
@@ -6,18 +6,19 @@ my_dir="$(dirname $my_file)"
 source $my_dir/functions
 
 # provision machines
-provision_machines 0 1 2 3 4
+provision_machines 0 1 2 3 4 5 6 7
 
 # prepare fuel master
 prepare_fuel_master 0
 
 # 3+1 cluster with default parameters:
-#   zero-padding=false
-#   TODO: add more parameters and check their default value
 configure_cluster mode 1 primary-controller 1 compute 2,3
+# Adding compute node to check that it will get to the same protection domain (parameter protection-domain-nodes is 100 by default)
+configure_cluster mode 1 primary-controller 1 compute 2,3,4
 
 check_fuel_perfomance 1
 check_protection_domain 1 'default'
+check_protection_domain_nodes 1 '100'
 check_sds_storage_pool 1 'default' "$device_paths"
 check_storage_pool 1 'Zero padding' 'disabled'
 check_storage_pool 1 'Checksum mode' 'disabled'
@@ -27,10 +28,13 @@ check_capacity_alerts 1 '80' '90'
 check_storage_pool 1 'Flash Read Cache' "Doesn't use"
 check_sds_on_controller 1 'true'
 
-remove_node_service 1 2 3
+remove_node_service 1 2 3 4
+
 new_storage_pools='sp1,sp2'
 new_device_paths='/dev/xvdf,/dev/xvdg'
+
 set_fuel_options protection-domain='pd'
+set_fuel_options protection-domain-nodes='3'
 set_fuel_options storage-pools=$new_storage_pools
 set_fuel_options device-paths=$new_device_paths
 set_fuel_options password='Other_password'
@@ -44,9 +48,11 @@ set_fuel_options cached-storage-pools='sp2'
 set_fuel_options rfcache-devices=$rfcache_paths
 set_fuel_options sds-on-controller='false'
 configure_cluster mode 1 primary-controller 1 compute 2,3,4
+configure_cluster mode 1 primary-controller 1 compute 2,3,4,5,6,7
 
 check_password 1 'Other_password'
 check_protection_domain 1 'pd'
+check_protection_domain_nodes 1 '3'
 check_sds_storage_pool 1 "$new_storage_pools" "$new_device_paths"
 check_storage_pool 1 'Zero padding' 'enabled'
 check_storage_pool 1 'Checksum mode' 'enabled'
