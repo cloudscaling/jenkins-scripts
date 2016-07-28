d70e2a060b6a2472bf36cd2fe55ed57f27fedfbe
Andrey Pavlov <andrey-mp@yandex.ru>
2016-07-28 16:03:31 +0300

    fuel ci: install needed packages before openstack check

--- fuel/check-openstack.sh
+++ fuel/check-openstack.sh
@@ -15,9 +15,10 @@ function get_provisioning_type() {
   awk '/provisioning_type:/ {print($2)}' /etc/astute.yaml 2>/dev/null
 }
 
+apt-get install -fqqy python-dev
 easy_install pip
 pip install -q virtualenv
 
 export SCALEIO_PROTECTION_DOMAIN='default'
 create_virtualenv
-run_os_checks exec_on_mdm get_provisioning_type 
+run_os_checks exec_on_mdm get_provisioning_type
