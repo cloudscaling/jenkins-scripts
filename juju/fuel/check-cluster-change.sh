a7051dd986b6366d8d2da763429a052a011ed0e2
tikita <rikitikitavi@yandex.ru>
2016-08-08 14:33:28 +0300

    master mdm is autodetected

--- juju/fuel/check-cluster-change.sh
+++ juju/fuel/check-cluster-change.sh
@@ -53,17 +53,18 @@ configure_cluster mode 5 primary-controller 1 compute 2,3 controller 4,5,6,7
 
 # 1+2 cluster
 echo "INFO: Switching cluster mode from 5 to 1"
-master_mdm=1
-cluster_output=`juju ssh ${machines[$master_mdm]} "sudo scli --query_cluster --approve_certificate" 2>/dev/null`
+old_master_mdm=`get_master_mdm ${machines[@]}`
+cluster_output=`juju ssh $old_master_mdm "sudo scli --query_cluster --approve_certificate" 2>/dev/null`
 slave_mdms_ip=(`echo "$cluster_output" | grep -A 10 "Slave MDMs:" | grep "Name:" | head -2 | awk '{gsub("[^0-9.]","",$2); print $2}'`)
 tie_breakers_ip=(`echo "$cluster_output" | grep -A 10 "Tie-Breakers:" | grep "Name:" | head -2 | awk '{gsub("[^0-9.]","",$2); print $2}'`)
 slave_mdms=()
 tie_breakers=()
 
-for mdm in 4 5 6 7 ; do
-  machine=${machines[$mdm]}
-  mdm_ip=`juju ssh $machine "ifconfig" 2>/dev/null | awk '/inet addr:/{print $2}' | head -1 | sed 's/addr://g'`
-  if [[ ${slave_mdms_ip[@]} =~ $mdm_ip ]] ; then
+for mdm in 1 4 5 6 7 ; do
+  mdm_ip=`juju ssh ${machines[$mdm]} "ifconfig" 2>/dev/null | awk '/inet addr:/{print $2}' | head -1 | sed 's/addr://g'`
+  if [[ ${machines[$mdm]} == $old_master_mdm ]] ; then
+    old_master_mdm_index=$mdm
+  elif [[ ${slave_mdms_ip[@]} =~ $mdm_ip ]] ; then
     slave_mdms+=($mdm)
   elif [[ ${tie_breakers_ip[@]} =~ $mdm_ip ]] ; then
     tie_breakers+=($mdm)
@@ -71,13 +72,13 @@ for mdm in 4 5 6 7 ; do
 done
 
 # 1+2 cluster
-remove_node_service 1 ${slave_mdms[1]}
+remove_node_service $old_master_mdm_index ${slave_mdms[1]}
 new_master_mdm=${slave_mdms[0]}
 configure_cluster mode 1 primary-controller $new_master_mdm compute 2,3
 remove_node_service "${tie_breakers[@]}"
 
 # 5+2 cluster
-configure_cluster mode 5 primary-controller $new_master_mdm compute 2,3 controller 1,${slave_mdms[1]},${tie_breakers[0]},${tie_breakers[1]}
+configure_cluster mode 5 primary-controller $new_master_mdm compute 2,3 controller $old_master_mdm_index,${slave_mdms[1]},${tie_breakers[0]},${tie_breakers[1]}
 
 
 # basic deploy 3+2 cluster
