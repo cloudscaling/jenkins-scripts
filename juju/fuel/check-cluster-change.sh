5d6519e4de953f3ef12e6eb8860ee09c242af7bc
tikitavi <rtikitavi@gmail.com>
2016-08-09 14:33:09 +0400

    improved output, tie-breakers doesnt remove in 5->1 mode (#51)
    * improved output, tie-breakers doesnt remove in 5->1 mode
    
    * fixed error
    
    * returned check of unbound variables

--- juju/fuel/check-cluster-change.sh
+++ juju/fuel/check-cluster-change.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -eux
+#!/bin/bash -eu
 
 # set this flag to true because fuel-plugin-scaleio is from master branch and dependent puppets also should be last version
 export PUPPET_DEV_MODE='true'
@@ -14,45 +14,44 @@ provision_machines 0 1 2 3 4 5 6 7
 # prepare fuel master
 prepare_fuel_master 0
 
-# 1+2 cluster
+echo "INFO: Configuring 1+2 cluster"
 configure_cluster mode 1 primary-controller 1 compute 2,3
 
-# 2+2 cluster
+echo "INFO: Configuring 2+2 cluster"
 configure_cluster mode 1 primary-controller 1 compute 2,3 controller 4
 
-# 3+2 cluster
+echo "INFO: 1->3 mode cluster"
 configure_cluster mode 3 primary-controller 1 compute 2,3 controller 4,5
 
-# 2+2 cluster
+echo "INFO: 3->1 mode cluster"
 remove_node_service 1
 configure_cluster mode 1 primary-controller 4 compute 2,3 controller 5
 
-# 3+2 cluster
+echo "INFO: 1->3 mode cluster"
 configure_cluster mode 3 primary-controller 4 compute 2,3 controller 1,5
 
-# 4+2 cluster
+echo "INFO: Configuring 4+2 cluster"
 configure_cluster mode 3 primary-controller 4 compute 2,3 controller 1,5,6
 
-# 5+2 cluster
+echo "INFO: 3->5 mode cluster"
 configure_cluster mode 5 primary-controller 4 compute 2,3 controller 1,5,6,7
 
-# 4+2 cluster
+echo "INFO: 5->3 mode cluster"
 remove_node_service 4
 configure_cluster mode 3 primary-controller 1 compute 2,3 controller 5,6,7
 
-# 3+2 cluster
+echo "INFO: Configuring 3+2 cluster"
 remove_node_service 5
 configure_cluster mode 3 primary-controller 1 compute 2,3 controller 6,7
 
-# 3+1 cluster
+echo "INFO: Configuring 3+1 cluster"
 remove_node_service 3
 configure_cluster mode 3 primary-controller 1 compute 2 controller 6,7
 
-# 5+2 cluster
+echo "INFO: 3->5 mode cluster"
 configure_cluster mode 5 primary-controller 1 compute 2,3 controller 4,5,6,7
 
-# 1+2 cluster
-echo "INFO: Switching cluster mode from 5 to 1"
+echo "INFO: 5->1 mode cluster"
 mdms=''
 for node in 1 4 5 6 7 ; do
   mdms+="${machines[$node]} "
@@ -79,17 +78,16 @@ done
 remove_node_service $current_master_mdm_index ${slave_mdms[1]}
 new_master_mdm=${slave_mdms[0]}
 configure_cluster mode 1 primary-controller $new_master_mdm compute 2,3
-remove_node_service "${tie_breakers[@]}"
 
-# 5+2 cluster
+echo "INFO: 1->5 mode cluster"
 configure_cluster mode 5 primary-controller $new_master_mdm compute 2,3 controller $current_master_mdm_index,${slave_mdms[1]},${tie_breakers[0]},${tie_breakers[1]}
 
 
-# basic deploy 3+2 cluster
+echo "INFO: Basic deploy 3+2 cluster"
 remove_node_service 1 2 3 4 5 6 7
 configure_cluster mode 3 primary-controller 1 compute 2,3 controller 4,5
 
-# basic deploy 5+2 cluster
+echo "INFO: Basic deploy 5+2 cluster"
 remove_node_service 1 2 3 4 5
 configure_cluster mode 5 primary-controller 1 compute 2,3 controller 4,5,6,7
 
