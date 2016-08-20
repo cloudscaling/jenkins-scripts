6e7537db0b6b0409d39f72a4d2537d9e9beca410
alexey-mr <alexey.morlang@gmail.com>
2016-08-20 12:59:49 +0300

    [fuel ci] add retries in scli calls

--- fuel/check-openstack.sh
+++ fuel/check-openstack.sh
@@ -7,8 +7,23 @@ source $my_dir/openrc
 source /etc/environment
 source $my_dir/functions-openstack
 
+retry_limit=3
+
 function exec_on_mdm() {
   bash -c "$@"
+}
+
+function exec_on_mdm_with_retry () {
+  count=1
+  while ! exec_on_mdm "$@" ; do
+    echo "INFO: try ${count} faled (cmd: ${@})"
+    if [[ $count == $retry_limit ]] ; then
+      echo 'ERROR: number of retries is exceeded'
+      return 1
+    fi
+    sleep 1
+    (( ++count ))
+  done
 }
 
 function get_provisioning_type() {
@@ -16,4 +31,4 @@ function get_provisioning_type() {
 }
 
 export SCALEIO_PROTECTION_DOMAIN='default'
-run_os_checks exec_on_mdm get_provisioning_type
+run_os_checks exec_on_mdm_with_retry get_provisioning_type
