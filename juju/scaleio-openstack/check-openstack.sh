5f58d9f1ff705e70b4e52fede4f0ea8323d7cd6d
alexey-mr <alexey.morlang@gmail.com>
2016-08-20 12:14:56 +0300

    remove get_volume_by_instance because it is not used

--- juju/scaleio-openstack/check-openstack.sh
+++ juju/scaleio-openstack/check-openstack.sh
@@ -16,22 +16,6 @@ function get_provisioning_type() {
   juju get scaleio-openstack | grep -A 15 provisioning-type | grep "value:" | head -1 | awk '{print $2}'
 }
 
-function get_volume_by_instance() {
-  instance="$1"
-
-  nodes=`juju status nova-compute --format tabular | awk '/nova-compute\//{print $5}'`
-  for node in $nodes ; do
-    if ! output=`juju ssh $node "virsh domblklist $instance" 2>/dev/null` ; then
-      continue
-    fi
-    vol_id=`echo "$output" | grep -Po "emc-vol-[0-9a-zA-Z]*-[0-9a-zA-Z]*" | cut -d '-' -f 4`
-    echo $vol_id
-    return 0
-  done
-
-  return 1
-}
-
 echo "INFO: Master MDM found at $master_mdm"
 auth_ip=`get_machine_ip keystone`
 export OS_AUTH_URL=http://$auth_ip:5000/v2.0
@@ -41,4 +25,4 @@ export OS_PROJECT_NAME=admin
 export OS_PASSWORD=password
 
 create_virtualenv
-run_os_checks exec_on_mdm get_provisioning_type get_volume_by_instance
+run_os_checks exec_on_mdm get_provisioning_type
