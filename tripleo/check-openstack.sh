11520f8ba8102e8c60ff333f4bbf4f0285b63156
Andrey Pavlov <andrey.mp@gmail.com>
2016-10-15 21:01:53 +0300

    add tripleo checks

---
+++ tripleo/check-openstack.sh
@@ -0,0 +1,33 @@
+#!/bin/bash -e
+
+my_file="$(readlink -e "$0")"
+my_dir="$(dirname $my_file)"
+
+sudo yum install -y wget gcc python-devel
+sudo easy_install pip
+sudo pip install virtualenv
+
+source $my_dir/../juju/functions
+source $my_dir/../juju/functions-openstack
+
+. ~/stackrc
+master_mdm=`nova list | grep controller-0 | awk '{print $12}' | cut -d '=' -f 2`
+
+. ~/overcloudrc
+if ! neutron net-list | grep net04 ; then
+  neutron net-create net04
+  neutron subnet-create --gateway 10.1.0.1 net04 10.1.0.0/24
+fi
+
+function exec_on_mdm() {
+  ssh heat-admin@$master_mdm "$@"
+}
+
+function get_provisioning_type() {
+  echo 'thin'
+}
+
+echo "INFO: Master MDM found at $master_mdm"
+
+create_virtualenv
+run_os_checks exec_on_mdm get_provisioning_type
