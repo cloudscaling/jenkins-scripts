0aa671ea643d6746d01c686ee0a364e33f74e232
alexey-mr <alexey.morlang@gmail.com>
2016-11-14 15:07:14 +0300

    [fuel ci] update cleanup script

--- fuel/cleanup_env.sh
+++ fuel/cleanup_env.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-set -eux
+set -ex
+
+if [[ -z "$1" ]] ; then
+  echo Environment number should be the first argument
+  exit -1
+fi
+env_number=$1
 
 my_dir="$(dirname "$0")"
 
@@ -9,7 +15,8 @@ if [[ "`whoami`" != 'root' ]] ; then
   exit -1
 fi
 
-pushd ${WORKSPACE}/fuel-kvm
-./manage.sh cleanup
-popd
+
+virsh list --all | awk "/fuel-[a-z]+-[$env_number].*/ {print \$2}" | xargs -i virsh destroy {}
+virsh list --all | awk '/fuel-[a-z]+-[$env_number].*/ {print \$2}' | xargs -i virsh undefine {}
+virsh vol-list --pool fuel-images | awk '/fuel-[a-z]+-[$env_number].*/ {print \$1}' | xargs -i virsh vol-delete --pool fuel-images  {}
 
